a8fec5a2add5634c619220ab56da73232d183594
alppinsaja <alvin@Alvins-MacBook-Pro.local>
2014-01-18 16:56:12 +0700

    4

--- js/index.js
+++ js/index.js
@@ -58,22 +58,22 @@ var app = {
 
         scanner.scan( function (result) { 
 
-            alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
-
-           console.log("Scanner result: \n" +
-                "text: " + result.text + "\n" +
-                "format: " + result.format + "\n" +
-                "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
-            console.log(result);
-            /*
-            if (args.format == "QR_CODE") {
-                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+            if (result.cancelled == false) {
+                $.ajax({
+                    url:'http://www.ind1sia.com/bar/test.php',
+                    type:'POST',
+                    data: {'key' : result.text},
+                    error : function(xhr, type) {
+                        alert('server error occurred');
+                    },
+                    timeout:60000,
+                    success:function(data){
+                        alert(data);
+                    }
+                });
+            } else {
+                alert("Canceled");
             }
-            */
 
         }, function (error) { 
             console.log("Scanning failed: ", error); 
